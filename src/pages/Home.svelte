f2f05f2bc8e1e6d219aa6d4b17724c5353b741c9
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-30 20:15:02 +0200

    updated Home.svelte template with new elements

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,3 +1,13 @@
 <script>
-    import FilterSort from "../components/"
+// @ts-nocheck
+
+    import FilterSort from "../components/FilterSort.svelte";
+    import ProductList from "../components/ProductList.svelte";
 </script>
+
+<main class="bg-[#81ccdd]">
+    <div>
+        <FilterSort />
+    </div>
+    <ProductList />
+</main>
